f707b5dca2bfd3d3d5077dc9d3eb0b91e54fe1ad
Anna Medyukhina <anna.medyukhina@gmail.com>
2022-07-08 13:47:10 -0500

    update setup

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup
+import filament_annotation_3d
 
 setup(
     name='filament_annotator_3d',
-    version='0.0',
+    version=filament_annotation_3d.__version__,
     author="Anna Medyukhina",
     author_email='anna.medyukhina@gmail.com',
-    packages=['filament_annotation_3d'
+    packages=['filament_annotation_3d',
+              'filament_annotation_3d.utils'
               ],
     license='Apache License 2.0',
     include_package_data=True,
