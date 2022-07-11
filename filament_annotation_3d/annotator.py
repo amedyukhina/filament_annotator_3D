29c05244f50f73845be16c497c2e9316abd758f3
Anna Medyukhina <anna.medyukhina@gmail.com>
2022-07-11 11:19:11 -0500

    fix bug in deleting last shape; improved comments

--- filament_annotation_3d/annotator.py
+++ filament_annotation_3d/annotator.py
@@ -41,23 +41,30 @@ def annotate_filaments(annotation_layer, output_fn, point_size=1,
     -------
 
     """
-    near_points = []
-    far_points = []
-    polygons = []
+    near_points = []  # store near points of the currently drawn polygon
+    far_points = []  # store far points of the currently drawn polygon
+    polygons = []  # store near and far points of the last 1-2 polygons (to compute intersections)
     img = None
-    if image_layer is not None:
-        img = image_layer.data.copy()
+
+    def __convert_sigma(sigma, shape):
+        """Convert the sigma value to fit the image shape"""
         sigma = np.ravel(sigma)
         if len(sigma) < 3:
             sigma = [sigma[0]] * 3
-        if len(img.shape) > 3:
-            sigma = [0] * (len(img.shape) - len(sigma)) + sigma
-        img = ndimage.gaussian_filter(img, sigma=sigma)
+        if len(shape) > 3:
+            sigma = [0] * (len(shape) - len(sigma)) + sigma
+        return sigma
+
+    if image_layer is not None:
+        img = image_layer.data.copy()
+        sigma = __convert_sigma(sigma, img.shape)
+        img = ndimage.gaussian_filter(img, sigma=sigma)  # smooth the image
 
     @annotation_layer.mouse_drag_callbacks.append
     def draw_polygon_shape(layer, event):
         """
-        Draw two polygons in different projections and calculate their intersection
+        Draw a line over the current 3D view of the filament.
+        Extend the line into a polygon restricted by the near and far intersection points.
         """
         yield
         if 'Control' in event.modifiers:  # draw a polygon if "Control" is pressed
@@ -72,8 +79,7 @@ def annotate_filaments(annotation_layer, output_fn, point_size=1,
                 near_points.append(near_point)
                 far_points.append(far_point)
 
-            # draw a polygon from the array of near and far points if there are > 3 of them
-
+            # draw a polygon from the array of near and far points
             if len(near_points) > 0 and len(far_points) > 0:
                 draw_polygon(layer, near_points.copy(), far_points.copy(), point_size=point_size)
 
@@ -82,16 +88,14 @@ def annotate_filaments(annotation_layer, output_fn, point_size=1,
     @annotation_layer.mouse_drag_callbacks.append
     def calculate_intersection(layer, event):
         """
-        Draw two polygons in different projections and calculate their intersection
+        Calculate the intersection of two polygons, if the length of the polygons array == 2.
         """
         yield
         while event.type == 'mouse_move':
-            if 'Control' not in event.modifiers:  # draw a polygon if "Control" is pressed
-                # add a polygon if there are some point saved
+            if 'Control' not in event.modifiers:  # Only execute if "Control" is not pressed
+                # add the last annotation to the polygon array and clear the annotation arrays
                 if len(near_points) > 0:
                     polygons.append([near_points.copy(), far_points.copy()])
-
-                # clear the points array
                 near_points.clear()
                 far_points.clear()
 
@@ -102,7 +106,8 @@ def annotate_filaments(annotation_layer, output_fn, point_size=1,
                     fpt1 = polygons[0][1]
                     fpt2 = polygons[1][1]
                     mt = compute_polygon_intersection(npt1, npt2, fpt1, fpt2)
-                    # add the calculated filament
+
+                    # snap to the brightest point
                     if image_layer is not None:
                         mt = np.array(mt)
                         if len(img.shape) > 3:
@@ -121,9 +126,8 @@ def annotate_filaments(annotation_layer, output_fn, point_size=1,
                     layer.add(mt, shape_type='path', edge_color='green', edge_width=point_size)
 
                     # clear the polygons array
-                    polygons[0] = None
-                    polygons[1] = None
-                    polygons.clear()
+                    polygons.pop()
+                    polygons.pop()
 
                     # save current annotations to a csv file
                     annotation_to_pandas(layer.data[1:]).to_csv(output_fn, index=False)
@@ -138,18 +142,26 @@ def annotate_filaments(annotation_layer, output_fn, point_size=1,
         """
         if layer.nshapes > 1:
             msg = 'delete the last added shape'
+
+            # delete the last shape in the annotation layer
             layer.selected_data = set(range(layer.nshapes - 1, layer.nshapes))
-            if len(polygons) > 0:
-                polygons.pop()
             layer.remove_selected()
+
+            if len(near_points) > 0:  # if any points in the near/far points array, clear them
+                near_points.clear()
+                far_points.clear()
+
+            elif len(polygons) > 0:  # otherwise, clear the polygons array
+                polygons.pop()
         else:
             msg = 'no shapes to delete'
+
         if show_message:
             layer.status = msg
             print(msg)
+
+        # save updated annotations to a csv file
         annotation_to_pandas(layer.data[1:]).to_csv(output_fn, index=False)
-        near_points.clear()
-        far_points.clear()
 
     @annotation_layer.bind_key('d')
     def delete_the_last_point(layer):
@@ -218,15 +230,21 @@ def draw_polygon(layer, near_points: list, far_points: list, color: str = 'red',
     -------
     Updated shapes layer
     """
-    if len(near_points) < 2:
+    if len(near_points) < 2:  # if only one point, add a temporary point for display purposes
         near_points.append(np.array(near_points[0]) + point_size)
         far_points.append(np.array(far_points[0]) + point_size)
+
+    # reverse the far points and combine near and far points into a polygon
     far_points_reverse = far_points.copy()
     far_points_reverse.reverse()
     polygon = np.array(near_points + far_points_reverse)
+
+    # remove the old polygon belonging to the same annotation
     if np.array((layer.data[-1][0] == polygon[0])).all():
         layer.selected_data = set(range(layer.nshapes - 1, layer.nshapes))
         layer.remove_selected()
+
+    # add the updated polygon
     layer.add(
         polygon,
         shape_type='polygon',
